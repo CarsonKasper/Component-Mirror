9a58c2e60ac0652b4a5e027a8e744a9e59c5501d
Carson Kasper <carson.akasper@gmail.com>
2025-03-27 10:35:00 -0700

    debugging close

--- Component Mirror.py
+++ Component Mirror.py
@@ -1,24 +1,7 @@
-# Assuming you have not changed the general structure of the template no modification is needed in this file.
-from . import commands
-from .lib import fusionAddInUtils as futil
-
+import commands
 
 def run(context):
-    try:
-        # This will run the start function in each of your commands as defined in commands/__init__.py
-        commands.start()
-
-    except:
-        futil.handle_error('run')
-
+    commands.start()
 
 def stop(context):
-    try:
-        # Remove all of the event handlers your app has created
-        futil.clear_handlers()
-
-        # This will run the start function in each of your commands as defined in commands/__init__.py
-        commands.stop()
-
-    except:
-        futil.handle_error('stop')
+    commands.stop()
